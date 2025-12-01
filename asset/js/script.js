537a942424f34b29b36500164a66b71b86ce4ee7
Unirions <64539568+Corentin-catteau@users.noreply.github.com>
2025-12-01 15:11:14 +0100

    replace e.code -> e.key

--- asset/js/script.js
+++ asset/js/script.js
@@ -30,7 +30,6 @@ var touches_P2 = [];
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('canvas_dessin');
     DrawGrid(canvas);
-
 });
 
 function init_Partie(id){
@@ -113,13 +112,13 @@ function commencer_Partie(){
     touches_P2 = [];
 
     document.addEventListener('keydown', function(e){
-        console.log(e.code);
-        if(Object.values(datas_Player1).includes(e.code)){
-            touches_P1.push(e.code);
+        console.log(e.key);
+        if(Object.values(datas_Player1).includes(e.key)){
+            touches_P1.push(e.key);
         }
 
-        if(Object.values(datas_Player2).includes(e.code)){
-            touches_P2.push(e.code);
+        if(Object.values(datas_Player2).includes(e.key)){
+            touches_P2.push(e.key);
         }
     });
 
@@ -129,7 +128,14 @@ function commencer_Partie(){
             partieEncours = false;
             return;
         }
+        if(datas_Player2.perdu === true){
+            clearInterval(gameLoop);
+            partieEnCours = false;
+            return;
+        }
+
         move_Player1(canvas, touches_P1.shift());
+        move_Player2(canvas, touches_P2.shift())
     }, 100);
 }
 
@@ -143,54 +149,74 @@ function move_Player1(canvas, latouche){
     changeDirection(datas_Player1, latouche);
     dessin_cercle(datas_Player1, ctx);
     verif_perdu_joueur1(canvas);
-
 }
 
-/*
 function move_Player2(canvas, latouche){
-    const stx = canvas.getContext('2d');
+    const ctx = canvas.getContext('2d');
 
-    ctx.fillStyle = "blue";
+    ctx.fillStyle = "white";
     ctx.fillRect(datas_Player2.position.x, datas_Player2.position.y, 10, 10);
     Set_Position_player2.add(`${datas_Player2.position.x}, ${datas_Player2.position.y}`)
 
-    changeDirection(latouche);
-    dessin_cercle(ctx);
+    changeDirection(datas_Player2, latouche);
+    dessin_cercle(datas_Player2, ctx);
     verif_perdu_joueur2(canvas);
 }
-*/
-function changeDirection(player, keyPressed){
-    var speed = 10;
-    let { x, y } = player.position;
-    let direction = player.direction;
 
-    if(keyPressed){
-        switch(keyPressed){
-            case player.up:
-                direction = 'top';
+function changeDirection(keyBindings, latouche){
+    var speed = 10
+    let { x, y} = keyBindings.position
+    let direction = keyBindings.direction
+
+    if(latouche){
+        switch(latouche){
+            case keyBindings.up:
+                direction = 'up'
                 break;
-            case player.down:
-                direction = 'down';
+            case keyBindings.down:
+                direction = 'down'
                 break;
-            case player.left:
-                direction = 'left';
+            case keyBindings.left:
+                direction = 'left'
                 break;
-            case player.right:
-                direction = 'right';
+            case keyBindings.right:
+                direction = 'right'
                 break;
+            case keyBindings.jump:
+                switch(direction){
+                    case 'up':
+                        y += speed
+                        break;
+                    case 'down':
+                        y -= speed
+                        break;
+                    case 'left':
+                        x -= speed
+                        break;
+                    case 'right':
+                        x += speed
+                        break;
+                }
         }
     }
-
     switch(direction){
-        case 'top': y -= speed; break;
-        case 'down': y += speed; break;
-        case 'left': x -= speed; break;
-        case 'right': x += speed; break;
+        case 'up':
+            y += speed
+            break;
+        case 'down':
+            y -= speed
+            break;
+        case 'left':
+            x -= speed
+            break;
+        case 'right':
+            x += speed
+            break;
     }
 
-    player.position.x = x;
-    player.position.y = y;
-    player.direction = direction;
+    keyBindings.position.x = x
+    keyBindings.position.y = y
+    keyBindings.direction = direction
 }
 
 function dessin_cercle(keyBindings, ctx){
@@ -198,12 +224,12 @@ function dessin_cercle(keyBindings, ctx){
     switch(keyBindings.direction){
         case 'top':
             cx = keyBindings.position.x + 5
-            cy = keyBindings.position.y
+            cy = keyBindings.position.y + 10
             ctx.arc(cx, cy, 5, Math.PI, 0, true);
             break;
         case 'down':
             cx = keyBindings.position.x + 5
-            cy = keyBindings.position.y + 10
+            cy = keyBindings.position.y
             ctx.arc(cx, cy, 5, 0, Math.PI, true); 
             break;
         case 'left':
@@ -224,8 +250,10 @@ function dessin_cercle(keyBindings, ctx){
 function verif_perdu_joueur1(canvas){
     const ctx = canvas.getContext('2d');
     
-    if(datas_Player1.perdu) 
-        if(!partieEnCours) return;
+    if(datas_Player1.perdu){
+        if(!partieEnCours) { return; }
+    }
+        
 
     if(Set_Position_player1.has(`${datas_Player1.position.x}, ${datas_Player1.position.y}`) ||
         datas_Player1.position.x > 800 ||
@@ -235,13 +263,41 @@ function verif_perdu_joueur1(canvas){
     ){
         datas_Player1.perdu = true;
     }
+    if(Set_Position_player2.has(`${datas_Player1.position.x}, ${datas_Player1.position.y}`)){
+        datas_Player1.perdu = true
+    }
 
     if(datas_Player1.perdu){
         finDeManche(canvas);
         return;
     }
     Set_Position_player1.add(`${datas_Player1.position.x}, ${datas_Player1.position.y}`);
-}  
+}   
+
+function verif_perdu_joueur2(canvas){
+    const ctx = canvas.getContext('2d');
+
+    if(datas_Player2.perdu)
+        if(!partieEnCours) return ;
+
+    if(Set_Position_player2.has(`${datas_Player2.position.x}, ${datas_Player2.position.y}`) || 
+        datas_Player2.position.x > 800 ||
+        datas_Player2.position.y > 590 ||
+        datas_Player2.position.x < 0 ||
+        datas_Player2.position.y < 0
+    ){
+        datas_Player2.perdu = true;
+    }
+    if(Set_Position_player1.has(`${datas_Player2.position.x}, ${datas_Player2.position.y}`)){
+        datas_Player2.perdu = true
+    }
+
+    if(datas_Player2.perdu){
+        finDeManche(canvas);
+        return;
+    }
+    Set_Position_player2.add(`${datas_Player2.position.x}, ${datas_Player2.position.y}`);
+}
 
 function finDeManche(canvas){
     let pts1 = document.getElementById('pts1');
@@ -253,9 +309,13 @@ function finDeManche(canvas){
 
     clearInterval(gameLoop);
 
-    alert("Joueur 1 a perdu !");
-    let pts2 = document.getElementById('pts2');
-    pts2.textContent = parseInt(pts2.textContent) + 1;
+    if(datas_Player1.perdu){
+        alert("Joueur 1 a perdu la manche !");
+        pts2.textContent = parseInt(pts2.textContent) + 1;
+    }else{
+        alert("Joueur 2 a perdu la manche !");
+        pts1.textContent = parseInt(pts1.textContent) + 1;
+    }
 
     if(pts1.textContent == 3){
         alert("Le joueur 1 à gagner la partie !!!");
@@ -271,8 +331,9 @@ function finDeManche(canvas){
     commencer_Partie();
 }
 
+//Touches
 document.addEventListener('keydown', function(e){
-    const key = e.key.toUpperCase(); // utilise la valeur du clavier
+    const key = e.key.toUpperCase();
     if([datas_Player1.up, datas_Player1.down, datas_Player1.left, datas_Player1.right, datas_Player1.jump].includes(key)){
         touches_P1.push(key);
     }
@@ -329,6 +390,7 @@ function changerTouche(joueur, action) {
 
     alert("Touche changée ! Maintenant tu joues avec " + touche);
 }
+
 //Mondal
 function ouvrirPlus(id) {
     document.getElementById(id).style.display = "flex";
